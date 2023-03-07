60f11d205f94cab3a0d96169c68437df0fa49e65
Ethan <elnair122@gmail.com>
2023-03-07 13:58:21 -0500

    db.js created

---
+++ db.js
@@ -0,0 +1,7 @@
+const { DataStore } = require("notarealdb");
+// not a real db docs: https://www.npmjs.com/package/notarealdb
+// "A 'fake' database for Node.js that stores data in local JSON files, for testing and sample applications"
+const store = new DataStore("./data");
+module.exports = {
+  students: store.collection("students"),
+};
